d31d89762c5b497ae303587bf28cf20ee0504e90
WESt <WESt@domaim.com>
2019-07-14 15:30:06 +0300

    OneDimArraysTaskOne completed.

--- src/by/epam/lab/training/algorithmization/OneDimArraysTaskOne.java
+++ src/by/epam/lab/training/algorithmization/OneDimArraysTaskOne.java
@@ -1,5 +1,7 @@
 package by.epam.lab.training.algorithmization;
 
+import java.util.Scanner;
+
 public class OneDimArraysTaskOne {
 
     /*
@@ -10,7 +12,21 @@ public class OneDimArraysTaskOne {
     1. В массив A [N] занесены натуральные числа.
        Найти сумму тех элементов, которые кратны данному К.
     */
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            final int[] DEFAULT_ARRAY = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            System.out.print("Input a number K: ");
+            int numberK = scanner.nextInt();
+            int sum = 0;
+
+            for (int currentElement : DEFAULT_ARRAY) {
+                if (currentElement % numberK == 0) {
+                    sum += currentElement;
+                }
+            }
+            System.out.println(sum != 0 ? "sum = " + sum : "No array elements found.");
+        }
     }
+
 }
