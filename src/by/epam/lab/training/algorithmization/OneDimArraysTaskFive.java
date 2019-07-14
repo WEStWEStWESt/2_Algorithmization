1e642229b267244ed2d1355c2141bfd84ed64c7e
WESt <WESt@domaim.com>
2019-07-14 18:44:59 +0300

    OneDimArraysTaskFive completed.

---
+++ src/by/epam/lab/training/algorithmization/OneDimArraysTaskFive.java
@@ -0,0 +1,27 @@
+package by.epam.lab.training.algorithmization;
+
+import java.util.Arrays;
+
+public class OneDimArraysTaskFive {
+
+    /*
+     2. Algorithmization
+
+       Одномерные массивы
+
+     5. Даны целые числа а 1 ,а 2  ,..., а n  .
+        Вывести на печать только те числа, для которых аi > i.*/
+
+    public static void main(String[] args) {
+        final int[] DEFAULT_ARRAY = {-1, 3, 0, 0, 48, -668, 0, -2, 1000, -1};
+
+        System.out.print("Elements: ");
+        for (int i = 0; i < DEFAULT_ARRAY.length; i++) {
+            int currentElement = DEFAULT_ARRAY[i];
+            if (currentElement > i) {
+                System.out.print(currentElement + " ");
+            }
+        }
+    }
+
+}
