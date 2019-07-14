3fc7f70ebb23a07bd5e0841b57a0df01cc0228a8
D-CURT <https://github.com/D-CURT>
2019-07-14 14:47:08 +0300

    OneDimArraysTaskOne empty class added.

---
+++ src/by/epam/lab/training/algorithmization/OneDimArraysTaskOne.java
@@ -0,0 +1,16 @@
+package by.epam.lab.training.algorithmization;
+
+public class OneDimArraysTaskOne {
+
+    /*
+    2. Algorithmization
+
+       Одномерные массивы
+
+    1. В массив A [N] занесены натуральные числа.
+       Найти сумму тех элементов, которые кратны данному К.
+    */
+    public static void main(String[] args) {
+
+    }
+}
