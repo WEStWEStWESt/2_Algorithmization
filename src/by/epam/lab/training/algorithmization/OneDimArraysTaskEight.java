adf285dc0090e9a01e2f4ccfac714083c3ab6acb
WESt <WESt@domaim.com>
2019-07-20 16:26:48 +0300

    OneDimArraysTaskEight completed.

--- src/by/epam/lab/training/algorithmization/OneDimArraysTaskEight.java
+++ src/by/epam/lab/training/algorithmization/OneDimArraysTaskEight.java
@@ -1,5 +1,8 @@
 package by.epam.lab.training.algorithmization;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class OneDimArraysTaskEight {
 
     /*
@@ -13,6 +16,34 @@ public class OneDimArraysTaskEight {
 
     public static void main(String[] args) {
 
+        final int[] DEFAULT_ARRAY = {-1, -2, 0, 0, 6, 7, 7, -2, -2};
+        int[] results;
+        int resultsLength = 0;
+        int resultsIndex = -1;
+        int min = DEFAULT_ARRAY[0];
+
+        for (int i = 1; i < DEFAULT_ARRAY.length; i++) {
+            if (DEFAULT_ARRAY[i] < min) {
+                min = DEFAULT_ARRAY[i];
+            }
+        }
+        for (int currentElement : DEFAULT_ARRAY) {
+            if (currentElement != min) {
+                resultsLength++;
+            }
+        }
+        results = new int[resultsLength];
+        for (int currentElement : DEFAULT_ARRAY) {
+            if (currentElement != min) {
+                results[++resultsIndex] = currentElement;
+            }
+        }
+
+        boolean replaced = resultsLength != 0;
+        if (replaced) {
+            System.out.println("The array before replacement: " + Arrays.toString(DEFAULT_ARRAY));
+        }
+        System.out.println(replaced ? "Replacements = " + Arrays.toString(results) : "No array elements replaced.");
     }
 
 }
