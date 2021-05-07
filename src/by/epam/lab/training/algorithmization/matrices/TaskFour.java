bba706a231cf266dd5edac2fbc941a9091d980d2
WESt <WESt>
2021-05-07 23:12:22 +0300

    ...

--- src/by/epam/lab/training/algorithmization/matrices/TaskFour.java
+++ src/by/epam/lab/training/algorithmization/matrices/TaskFour.java
@@ -1,5 +1,6 @@
 package by.epam.lab.training.algorithmization.matrices;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TaskFour {
@@ -22,18 +23,29 @@ public class TaskFour {
             System.out.print("Enter an even size of the matrix: ");
             int size = scanner.nextInt();
 
-            while (size%2 !=0){
-                System.out.print("Incorrect! Enter an even number: ");
+            while (size%2 !=0 || size < 2){
+                System.out.print("Incorrect! Enter a positive even number>1 : ");
                 size = scanner.nextInt();
             }
+           //matrix[i][j]=(int) (Math.random() * (size-1) + 1);
 
-            /*int[][] MATRIX = new int[size][size];
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-
+            int[][] matrix = new int[size][size];
+            //int col = 0, col2 = 0, row = 0, row2 =0;
+            for (int i = 0; i <= size-1; i++) {
+               // col = i+1;
+                for (int j = 0; j <= size-1; j++) {
+                    /*row = j+1;
+                    matrix[col2][row2] = matrix[i][j];
+                    col = row =0;*/
                 }
-            }*/
+            }
 
+            for (int i = 0; i <= size-1; i++) {
+                for (int j = 0; j <= size-1; j++) {
+                    System.out.print(matrix[i][j] + "  ");
+                }
+                System.out.println();
+            }
         }
     }
 }
