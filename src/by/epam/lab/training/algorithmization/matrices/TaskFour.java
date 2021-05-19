62a062420592935d7220083c80fede2a20b205a3
WESt <WESt>
2021-05-19 23:18:30 +0300

    very bad code ready(matrices\taskFour)

--- src/by/epam/lab/training/algorithmization/matrices/TaskFour.java
+++ src/by/epam/lab/training/algorithmization/matrices/TaskFour.java
@@ -19,33 +19,38 @@ public class TaskFour {
     */
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)){
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter an even size of the matrix: ");
             int size = scanner.nextInt();
 
-            while (size%2 !=0 || size < 2){
+            while (size % 2 != 0 || size < 2) {
                 System.out.print("Incorrect! Enter a positive even number>1 : ");
                 size = scanner.nextInt();
             }
-           //matrix[i][j]=(int) (Math.random() * (size-1) + 1);
-
+            //matrix[i][j]=(int) (Math.random() * (size-1) + 1);
             int[][] matrix = new int[size][size];
-            //int col = 0, col2 = 0, row = 0, row2 =0;
-            for (int i = 0; i <= size-1; i++) {
-               // col = i+1;
-                for (int j = 0; j <= size-1; j++) {
-                    /*row = j+1;
-                    matrix[col2][row2] = matrix[i][j];
-                    col = row =0;*/
-                }
+            int var = 0;
+            int count = 0;
+            for (int i = 0; i < size/2 ; ++var) {
+                    for (int k = 0; k < size; k++) {
+                        System.out.print("__");
+                        System.out.print(matrix[i][k] = i + ++k);
+                        --k;
+                    }
+                    System.out.println("");
+                    for (int j = size; j > 0; --j) {
+                        System.out.print("..");
+                        System.out.print(matrix[--j][i] = j + ++i);
+                        ++j;
+                        --i;
+                    }
+                    System.out.println("");
+                    i++;
+                    var=--i;
+                    count++;
+                    if (count == size/2) break;
             }
-
-            for (int i = 0; i <= size-1; i++) {
-                for (int j = 0; j <= size-1; j++) {
-                    System.out.print(matrix[i][j] + "  ");
-                }
-                System.out.println();
-            }
+            System.out.println("END");
         }
     }
 }
